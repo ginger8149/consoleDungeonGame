d60c2aa3af1cf2ea961d331b36693db27e2de04d
Ginger8149 <cs8149@outlook.com>
2024-06-18 11:49:58 +0100

    created blank render functions

---
+++ consoleDungeonGame/consoleDungeonGame/UIManagment/uiRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleDungeonGame.UIManagment
+{
+    internal class uiRenderer
+    {
+
+        public void renderGameScreen()
+        {
+
+        }
+        public void renderDungeonRoom() 
+        {
+
+        }
+        public void renderDungeonMap()
+        {
+
+        }
+
+
+
+
+    }
+}
