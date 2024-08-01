bae55f4d89db17db2a75330a12057ea603f4b30d
Ginger8149 <cs8149@outlook.com>
2024-08-01 22:31:32 +0100

    started working on dungon managment scripts

---
+++ consoleDungeonGame/consoleDungeonGame/GameManagment/dungeonManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleDungeonGame.GameManagment
+{
+    internal class dungeonManager
+    {
+
+
+        public dungeonManager() { }
+
+        public void loadDungeonFromSave()
+        {
+
+        }
+
+        public void saveDungeonToDisk() { }
+
+        public void generateNewDungeon(int difficulty)
+        {
+
+
+
+
+
+
+        }
+
+    }
+
+}
